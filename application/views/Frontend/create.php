9c6aa5391843cbfd7aa7c25c37a24df3986eccd4
Sami Sardarzai <cleversam07@gmail.com>
2023-10-02 05:29:48 -0700

    style added

--- application/views/Frontend/create.php
+++ application/views/Frontend/create.php
@@ -1,1 +1,42 @@
-sam
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-4">
+            <div class="card">
+                <div class="card-header ">
+                    <h5>
+                        How to insert data into database
+                        
+                    </h5>
+                    <a  href="<?php echo base_url('employee');?>  " class="btn btn-danger float-xl-right float-right "> Back</a>
+                </div>
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="">First Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Last Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Phone Number</label>
+                            <input type="text" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                           <button type="submit" class="btn btn-primary mt-4 ">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
