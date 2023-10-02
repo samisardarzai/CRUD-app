f5d52da2679cb2cd36902a80a60ac70f333d7484
Sami Sardarzai <cleversam07@gmail.com>
2023-10-02 02:54:14 -0700

    add employee

---
+++ application/views/Frontend/employee.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5>
+                        How to insert data into database
+                        <a href="<?php echo base_url('employee/add');?> " class="btn btn-primary flout-right">Add Employee</a>
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
